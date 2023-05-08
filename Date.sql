1b579f1e96233d77d3709052ee44eb34666de806
HarshWork99 <hsharma7991@gmail.com>
2023-05-08 19:38:23 +0530

    Initial commit

---
+++ Date.sql
@@ -0,0 +1,25 @@
+-- Cleansed DIM_Date Table --
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  --[DayNumberOfWeek], 
+  [EnglishDayNameOfWeek] AS Day, 
+  --[SpanishDayNameOfWeek], 
+  --[FrenchDayNameOfWeek], 
+  --[DayNumberOfMonth], 
+  --[DayNumberOfYear], 
+  --[WeekNumberOfYear],
+  [EnglishMonthName] AS Month, 
+  Left([EnglishMonthName], 3) AS MonthShort,   -- Abrevatted month column for graphs
+  --[SpanishMonthName], 
+  --[FrenchMonthName], 
+  [MonthNumberOfYear] AS Month_No, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year
+  --[FiscalQuarter], 
+  --[FiscalYear], 
+  --[FiscalSemester] 
+FROM 
+ [AdventureWorksDW2019].[dbo].[DimDate]
+WHERE 
+  CalendarYear >= 2021
